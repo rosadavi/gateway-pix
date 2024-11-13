bc74f552e8cc7c4594eee0ab2ba56a8cbbb078d0
rosadavi <davi.rosa.ofc@gmail.com>
2024-11-13 14:59:25 -0300

    cryptando senha e token

--- system/backend/src/middlewares/jwt.ts
+++ system/backend/src/middlewares/jwt.ts
@@ -1,12 +1,14 @@
 import jwt from "jsonwebtoken";
+import { hashPayload } from "../configs/bcrypt.js";
 import { JwtPayload } from "../configs/token.js";
 import dotenv from "dotenv";
 dotenv.config();
 
 const JWT_SECRET = process.env.JWT_SECRET!;
 
-export function generateToken(payload: JwtPayload): string {
-    return jwt.sign(payload, JWT_SECRET);
+export async function generateToken(payload: JwtPayload): Promise <string> {
+    const payloadHash = await hashPayload(payload);
+    return jwt.sign({ payloadHash }, JWT_SECRET);
 }
 
 export function validateToken(token: string): JwtPayload | null {
--- system/backend/src/services/LoginService.ts
+++ system/backend/src/services/LoginService.ts
@@ -1,6 +1,7 @@
 import prismaClient from "../prisma";
 import { generateToken } from '../middlewares/jwt.js';
 import { JwtPayload } from "../configs/token";
+import { compareHashSenha } from "../configs/bcrypt";
 
 
 interface LoginProps {
@@ -22,22 +23,29 @@ export class LoginService {
             const fetchUser = await prismaClient.vw_empresa.findFirst({
                 where: {
                     cpf_cnpj_empresa: cnpj_cpf,
-                    senha_empresa: senha
                 },
                 select: {
                     id_empresa: true,
-                    cpf_cnpj_empresa: true
+                    cpf_cnpj_empresa: true,
+                    senha_empresa: true
                 }
             });
             if(fetchUser) {
-                const token = createToken({
-                    cpf_cnpj_empresa: fetchUser.cpf_cnpj_empresa ?? '',
-                    id_empresa: fetchUser.id_empresa ??''
-                });
+                const senhaValida = await compareHashSenha(senha, fetchUser.senha_empresa!);
+
+                if(senhaValida) {
+                    const token = createToken({
+                        cpf_cnpj_empresa: fetchUser.cpf_cnpj_empresa ?? '',
+                        id_empresa: fetchUser.id_empresa ??''
+                    });
                     return token;
-                 } else {
-                     return  'Credenciais Invalidas!';
-                 }
+                } else {
+                    return  'Credenciais Invalidas!';
+                    
+                }
+            } else {
+                return 'Credenciais Invalidas!';
+            }
 
         } catch(error) {
             console.error("Erro ao realizar login: ", error);
