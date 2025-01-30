f63603f4300ed53617df00c7f9a507bf6241e226
MBareicha <matheusbareich@gmail.com>
2025-01-30 19:22:21 -0300

    Preparacao para https

--- system/backend/src/server.ts
+++ system/backend/src/server.ts
@@ -1,11 +1,18 @@
 import express from 'express';
 import routes from './routes.js';
 import dotenv from 'dotenv';
+import https from 'https';
+import fs from 'fs';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+
+const options = {
+  key: fs.readFileSync('./certs/server.key'),
+  cert: fs.readFileSync('./certs/server.cert')
+};
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -16,3 +23,6 @@ app.use('/', routes);
 app.listen(PORT, () => {
   console.log(`Server is running at ${PORT}`);
 });
+//https.createServer(options, app).listen(PORT, () => {
+//  console.log(`Server is running at ${PORT}`);
+//});
