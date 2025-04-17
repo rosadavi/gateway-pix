6418d79195da259d4f47e6d465b7cc0c14ac995c
rosa <davi.rosa.ofc@gmail.com>
2025-04-17 16:58:28 -0300

    padronizando nome das rotas para class/acao

--- system/backend/src/routes.ts
+++ system/backend/src/routes.ts
@@ -23,19 +23,19 @@ router.post('/verifica', authMiddleware, (req: Request, res: Response) => {
    new VerificaCPFeCNPJController().handle(req, res);
 });
 
-router.post('/criar/proprietario', (req: Request, res: Response) => {
+router.post('/empresa/cadastrar', (req: Request, res: Response) => {
   new CriarProprietarioController().handle(req, res);
 });
 
-router.post('/login/proprietario', (req: Request, res: Response) => {
+router.post('/empresa/cadastrar', (req: Request, res: Response) => {
   new LoginProprietarioController().handle(req, res);
 });
 
-router.post('/cobranca', authMiddleware,(req: Request, res: Response) => {
+router.post('/cobranca/gerar', authMiddleware,(req: Request, res: Response) => {
   new GeraCobrancaController().handle(req, res);
 });
 
-router.post('/criar/cobranca/itens', authMiddleware, (req: Request, res: Response) => {
+router.post('/cobranca/gerar/itens', authMiddleware, (req: Request, res: Response) => {
   new CriarCobrancaItensController().handle(req, res);
 });
 
@@ -43,27 +43,27 @@ router.get('/validar', authMiddleware, (req: Request, res: Response) => {
   res.status(200).json({message: "Autorizado"});
 });
 
-router.post('/extrato', authMiddleware, (req: Request, res: Response) => {
+router.post('/extrato/gerar', authMiddleware, (req: Request, res: Response) => {
   new GeraExtratoController().handle(req, res);
 });
 
-router.post('/criar/extrato/detalhado', authMiddleware, (req: Request, res: Response) => {
+router.post('/extrato/gerar/detalhado', authMiddleware, (req: Request, res: Response) => {
   new CriarExtratoDetalhadoController().handle(req, res);
 });
 
-router.post('/criar/produto', authMiddleware, (req: Request, res: Response) => {
+router.post('/produto/cadastrar', authMiddleware, (req: Request, res: Response) => {
   new RegistrarProdutoController().handle(req, res);
 });
 
-router.post('/criar/produto/item', authMiddleware, (req: Request, res: Response) => {
+router.post('/produto/cadastrar/item', authMiddleware, (req: Request, res: Response) => {
   new CriarItemProdutoController().handle(req, res);
 });
 
-router.post('/criar/produto/categoria', authMiddleware, (req: Request, res: Response) => {
+router.post('/produto/cadastrar/categoria', authMiddleware, (req: Request, res: Response) => {
   new CriarCategoriaController().handle(req, res);
 });
 
-router.post('/transacao/criar', authMiddleware, (req: Request, res: Response) => {
+router.post('/transacao/cadastrar', authMiddleware, (req: Request, res: Response) => {
   new CriarTipoTransacaoController().handle(req, res);
 });
 
